2cef8b809068c4fc23773679a5f86a82f4dc2614
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-02-07 17:57:22 +0100

    Added sample

---
+++ Sample/Tharga.ConsoleSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Tharga.Quilt4Net;
+
+namespace Tharga.ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quilt4Net.Configuration.ClientToken = "5H4ZP7EQJWPM2539782YACX6V57J62DF";
+            Quilt4Net.Configuration.Target.Location = "http://localhost:50154/";
+
+            Quilt4Net.Session.Register();
+
+            Quilt4Net.Issue.Register("ABC", Issue.MessageIssueLevel.Warning);
+
+            Quilt4Net.Session.End();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
